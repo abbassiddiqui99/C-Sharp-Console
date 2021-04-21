56e90af1f1893cf5e2f98efad5ea6bb4de8a8881
Abbas Hussain <abbassiddiqui99@gmail.com>
2021-04-22 03:40:04 +0500

    constructor is added with THIS keyword.

--- Program.cs
+++ Program.cs
@@ -6,26 +6,10 @@ namespace C_Sharp_Console
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Console.Write("Enter First Number: ");
-                int num1 = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Enter Second Number: ");
-                int num2 = Convert.ToInt32(Console.ReadLine());
-                System.Console.WriteLine(num1/num2);
-            }
-            catch (System.DivideByZeroException)
-            {
-                System.Console.WriteLine("Attempted to divide by zero");                
-            }
-            catch (System.FormatException)
-            {
-                System.Console.WriteLine("Wrong Input");                
-            }
-            finally {
-                System.Console.WriteLine("Always Executed");
-            }
-            
+            Book book_01 = new Book("Harry Potter","JK Rowling",400);
+            Book book_02 = new Book("Lord of the Rings","Tolkein",700);
+
+            System.Console.WriteLine(book_01.pages);
 
             Console.ReadLine();
         }
